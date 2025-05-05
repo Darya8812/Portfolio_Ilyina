ea9325f744086fc7f39591b46c6efee0caf60aa2
Darya8812 <115808845+Darya8812@users.noreply.github.com>
2025-05-05 21:25:56 +0300

    Create test_sql.py

---
+++ sales-analysis/tests/test_sql.py
@@ -0,0 +1,91 @@
+Общий объем продаж: 2261536.7827
+
+Топ-5 товаров по продажам:
+                                               Month  Total Sales
+0              Canon imageCLASS 2200 Advanced Copier    61599.824
+1  Fellowes PB500 Electric Punch Plastic Comb Bin...    27453.384
+2  Cisco TelePresence System EX90 Videoconferenci...    22638.480
+3       HON 5400 Series Task Chairs for Big and Tall    21870.576
+4         GBC DocuBind TL300 Electric Binding System    19823.479
+
+Средняя стоимость заказа: 230.76905945918367
+
+Количество заказов по категориям: [('Office Supplies', 5909), ('Furniture', 2078), ('Technology', 1813)]
+
+Продажи по месяцам:
+      Month  Total Sales
+0   2015-01   14205.7070
+1   2015-02    4519.8920
+2   2015-03   55205.7970
+3   2015-04   27906.8550
+4   2015-05   23644.3030
+5   2015-06   34322.9356
+6   2015-07   33781.5430
+7   2015-08   27117.5365
+8   2015-09   81623.5268
+9   2015-10   31453.3930
+10  2015-11   77907.6607
+11  2015-12   68167.0585
+12  2016-01   18066.9576
+13  2016-02   11951.4110
+14  2016-03   32339.3184
+15  2016-04   34154.4685
+16  2016-05   29959.5305
+17  2016-06   23599.3740
+18  2016-07   28608.2590
+19  2016-08   36818.3422
+20  2016-09   63133.6060
+21  2016-10   31011.7375
+22  2016-11   75249.3995
+23  2016-12   74543.6012
+24  2017-01   18542.4910
+25  2017-02   22978.8150
+26  2017-03   51165.0590
+27  2017-04   38679.7670
+28  2017-05   56656.9080
+29  2017-06   39724.4860
+30  2017-07   38320.7830
+31  2017-08   30542.2003
+32  2017-09   69193.3909
+33  2017-10   59583.0330
+34  2017-11   79066.4958
+35  2017-12   95739.1210
+36  2018-01   43476.4740
+37  2018-02   19920.9974
+38  2018-03   58863.4128
+39  2018-04   35541.9101
+40  2018-05   43825.9822
+41  2018-06   48190.7277
+42  2018-07   44825.1040
+43  2018-08   62837.8480
+44  2018-09   86152.8880
+45  2018-10   77448.1312
+46  2018-11  117938.1550
+47  2018-12   83030.3888
+
+Анализ прибыльности товаров
+                                        Product Name  Total Sales
+0              Canon imageCLASS 2200 Advanced Copier    61599.824
+1  Fellowes PB500 Electric Punch Plastic Comb Bin...    27453.384
+2  Cisco TelePresence System EX90 Videoconferenci...    22638.480
+3       HON 5400 Series Task Chairs for Big and Tall    21870.576
+4         GBC DocuBind TL300 Electric Binding System    19823.479
+5   GBC Ibimaster 500 Manual ProClick Binding System    19024.500
+6               Hewlett Packard LaserJet 3310 Copier    18839.686
+7  HP Designjet T520 Inkjet Large Format Printer ...    18374.895
+8          GBC DocuBind P400 Electric Binding System    17965.068
+9        High Speed Automatic Electric Letter Opener    17030.312
+
+Продажи по сегментам клиентов
+       Segment   Total Sales
+0     Consumer  1.148061e+06
+1    Corporate  6.884941e+05
+2  Home Office  4.249822e+05
+
+Динамика продаж по категориям
+          Category  Total Sales
+0       Technology  827455.8730
+1        Furniture  728658.5757
+2  Office Supplies  705422.3340
+
+Process finished with exit code 0
