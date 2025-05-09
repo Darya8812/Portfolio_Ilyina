cbda532d9716070f8f4f6306af706c551787fbc6
Darya8812 <115808845+Darya8812@users.noreply.github.com>
2025-05-09 17:26:27 +0300

    Add files via upload

---
+++ VideoGamesSales/data/setup_db.py
@@ -0,0 +1,25 @@
+import sqlite3
+import pandas as pd
+
+# Создаем базу SQLite
+conn = sqlite3.connect("sales.db")
+cursor = conn.cursor()
+
+# Загружаем CSV
+df = pd.read_csv("D:/sales_dataset.csv")
+
+# Преобразуем "Order Date" в нужный формат
+df["Order Date"] = pd.to_datetime(df["Order Date"], format="%d/%m/%Y").dt.strftime("%Y-%m-%d")
+
+# Перезаписываем таблицу в SQLite
+df.to_sql("sales_dataset", conn, if_exists="replace", index=False)
+
+# Сохраняем данные в таблицу SQL
+df.to_sql("sales_dataset", conn, if_exists="replace", index=False)
+
+# Проверяем, что данные загрузились
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+print("Список таблиц:", cursor.fetchall())  # Выведет все таблицы в базе
+
+
+conn.close()
